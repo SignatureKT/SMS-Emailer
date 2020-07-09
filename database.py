bdc5a0e066892d3fe418c4b5157ba93b5c1400e6
signaturekt <kevin97truong@gmail.com>
2020-07-09 11:07:56 -0700

    Creats a table with 'first_name', 'last_name', and 'email'

--- database.py
+++ database.py
@@ -1,5 +1,20 @@
 import sqlite3
 
+#create a connection
 conn = sqlite3.connect('customer.db')
 
-  
+#Create a cursor
+c = conn.cursor()
+
+#Create a table
+c.execute("""CREATE TABLE customers (
+        first_name text,
+        last_name text,
+        email text) """
+)
+
+#Commit our command
+conn.commit()
+
+#Commit our connection
+conn.close()
