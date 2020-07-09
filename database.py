4a4087ab5adc4ddbc38d5b4c3a4ec73af34ca1a9
signaturekt <kevin97truong@gmail.com>
2020-07-09 11:28:41 -0700

    Add a function called 'checkTableExist' to check if a table exist in the database

--- database.py
+++ database.py
@@ -1,5 +1,15 @@
 import sqlite3
 
+def checkTableExists(conn, tablename):
+    conn.execute("""
+        SELECT count(name)
+        FROM sqlite_master
+        WHERE type='table' AND name = '{0}'
+        """.format(tablename))
+    if conn.fetchone()[0] == 1:
+        return True
+    return False
+
 #create a connection
 conn = sqlite3.connect('customer.db')
 
@@ -7,11 +17,12 @@ conn = sqlite3.connect('customer.db')
 c = conn.cursor()
 
 #Create a table
-c.execute("""CREATE TABLE customers (
-        first_name text,
-        last_name text,
-        email text) """
-)
+if not checkTableExists(c, 'customers'):
+    c.execute("""CREATE TABLE customers (
+            first_name text,
+            last_name text,
+            email text) """
+    )
 
 #Commit our command
 conn.commit()
