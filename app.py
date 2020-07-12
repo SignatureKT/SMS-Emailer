c455cef860fd5bd664987c7ee1ef3d2e2074e667
signaturekt <kevin97truong@gmail.com>
2020-07-11 22:17:46 -0700

    add functions for users to query items based on the table's column name, and user's value.

--- app.py
+++ app.py
@@ -15,12 +15,6 @@ def deleteOneInput():
     print('Which id would you like to delete:')
     return input()
 
-exmapleCustomers = [
-    ('Jose', 'Moris', 'JoseMoris@example.com'),
-    ('Sara', 'Wills', 'SaraWills@example.com'),
-    ('June', 'Renning', 'JuneRenning@example.com')
-]
-
 def addMultiple():
     customerDict = []
     userInput = 'y'
@@ -34,6 +28,18 @@ def addMultiple():
         userInput = input().lower().strip()
     return customerDict
 
+def showInput():
+    print('What is the key you like to use to search (rowid, first_name, last_name, or email):')
+    key = input()
+    print('What is the value you would like to search:')
+    value = input()
+    return key, value
+
+exmapleCustomers = [
+    ('Jose', 'Moris', 'JoseMoris@example.com'),
+    ('Sara', 'Wills', 'SaraWills@example.com'),
+    ('June', 'Renning', 'JuneRenning@example.com')
+]
 
 print("To exit the program enter 'exit':")
 userInput = input().lower().strip()
@@ -45,10 +51,11 @@ while userInput != 'exit':
     elif userInput == 'delete':
         database.delete_one(deleteOneInput())
     elif userInput == 'show':
+        database.query(showInput())
+    elif userInput == 'showall':
         database.show_all()
     elif userInput == 'help':
-        print("The commands are 'exit', 'add', 'add+', 'show', and 'delete'")
+        print("The commands are 'exit', 'add', 'add+', 'showall', 'show', and 'delete'")
     else:
         print("Invalid Command. Type in 'help' to see list of commands")
     userInput = input().lower().strip()
-#database.add_many(exmapleCustomers)
