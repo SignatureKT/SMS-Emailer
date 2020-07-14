60c89f8d3d632a9e9d1be5123bb4bd77dd62e57d
signaturekt <kevin97truong@gmail.com>
2020-07-14 15:07:12 -0700

    Add user inputs into a seperate file

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import database, sys, subprocess, os
-
+import database, sys, subprocess, os, user
 commandsHelp = [
     'createtable - Creates a table in the database',
     'add - Creates a record in the table',
@@ -32,121 +31,43 @@ commands = [
     'exit'
 ]
 
-def main():
-    #recieve user input to create/open database
-    databaseName = getDatabase()
-    #if there is no table in the database, the user may create one
-    if not database.checkTableExistsDB(databaseName):
-        addTableOption(databaseName)
-    print("To exit the program enter 'exit':")
+#recieve user input to create/open database
+databaseName = user.getDatabase()
+#if there is no table in the database, the user may create one
+if not database.checkTableExistsDB(databaseName):
+    if user.addTableOption(databaseName) == True:
+        database.createTable(databaseName, user.getTableNameInput())
+print("To exit the program enter 'exit':")
+userInput = input('menu: ').lower().strip()
+while userInput != 'exit':
+    if userInput == commands[0]:
+        database.createTable(databaseName, user.getTableNameInput())
+    elif userInput == commands[1]:
+        database.add_one(databaseName, user.getTableNameInput(), user.addOneInput())
+    elif userInput == commands[2]:
+        database.add_many(databaseName, user.getTableNameInput(), user.addMultiple())
+    elif userInput == commands[3]:
+        tableName = user.getTableNameInput()
+        database.show_all(databaseName, tableName)
+        database.delete_one(databaseName, tableName, user.deleteOneInput(databaseName, tableName))
+    elif userInput == commands[4]:
+        database.query(databaseName, user.getTableNameInput(), user.showInput())
+    elif userInput == commands[5]:
+        database.show_all(databaseName, user.getTableNameInput())
+    elif userInput == commands[6]:
+        subprocess.run([sys.executable, f"./mails.py", f'{databaseName}', f'{user.getTableNameInput()}'])
+    elif userInput == commands[7]:
+        database.showTables(databaseName)
+    elif userInput == commands[8]:
+        database.deleteTable(databaseName, user.getTableNameInput())
+    elif userInput == commands[9]:
+        databaseName = user.getNewDatabaseInput()
+    elif userInput == commands[10]:
+        database.updateTable(databaseName, user.getTableNameInput(), user.updateInput())
+    elif userInput == commands[11]:
+        user.lineBreak()
+        for string in commandsHelp: print(f'{string}\n')
+        user.lineBreak()
+    else:
+        print("Invalid Command. Type in 'help' to see list of commands and usage")
     userInput = input('menu: ').lower().strip()
-    while userInput != 'exit':
-        if userInput == commands[0]:
-            database.createTable(databaseName, getTableNameInput())
-        elif userInput == commands[1]:
-            database.add_one(databaseName, getTableNameInput(), addOneInput())
-        elif userInput == commands[2]:
-            database.add_many(databaseName, getTableNameInput(), addMultiple())
-        elif userInput == commands[3]:
-            tableName = getTableNameInput()
-            database.delete_one(databaseName, tableName, deleteOneInput(databaseName, tableName))
-        elif userInput == commands[4]:
-            database.query(databaseName, getTableNameInput(), showInput())
-        elif userInput == commands[5]:
-            database.show_all(databaseName, getTableNameInput())
-        elif userInput == commands[6]:
-            subprocess.run([sys.executable, f"./mails.py", f'{databaseName}', f'{getTableNameInput()}'])
-        elif userInput == commands[7]:
-            database.showTables(databaseName)
-        elif userInput == commands[8]:
-            database.deleteTable(databaseName, getTableNameInput())
-        elif userInput == commands[9]:
-            databaseName = getNewDatabaseInput()
-        elif userInput == commands[10]:
-            database.updateTable(databaseName, getTableNameInput(), updateInput())
-        elif userInput == commands[11]:
-            lineBreak()
-            for string in commandsHelp: print(f'{string}\n')
-            lineBreak()
-        else:
-            print("Invalid Command. Type in 'help' to see list of commands and usage")
-        userInput = input('menu: ').lower().strip()
-
-#Get input from user
-def addOneInput():
-    print('What is the first name you want to put into the table:')
-    firstName = input('first name: ')
-    lastName = input('last name: ')
-    email = input('email: ')
-    phone = input('phone: ')
-    return firstName, lastName, email, phone
-
-#Get input from user
-def deleteOneInput(databaseName, tableName):
-    database.show_all(databaseName, tableName)
-    print('Which record would you like to delete:')
-    return input('rowid: ')
-
-#Creates a list and get multiple input from user
-def addMultiple():
-    customerDict = []
-    userInput = 'y'
-    while userInput != 'n':
-        if userInput == 'y':
-            customerDict.append(addOneInput())
-            print(customerDict)
-        else:
-            print("Invalid Prompt")
-        print('Continue?[Y/N]:')
-        userInput = input().lower().strip()
-    return customerDict
-
-#Get input for columns and value from user
-def showInput():
-    print('What is the key you like to use to search\n(rowid, first_name, last_name, email, or phone):')
-    key = input()
-    print('What is the value you would like to search:')
-    value = input()
-    return key, value
-
-def updateInput():
-    print('What is the key you like to use to search\n(rowid, first_name, last_name, email, or phone):')
-    key = input()
-    print('What rowid you would like to use to update you record:')
-    rowid = input('rowid:')
-    print('What would you like to change the value to:')
-    value = input('value: ')
-    return key, value, rowid
-
-#get input for tables
-def getTableNameInput():
-    print("Which table would you like to look/create at:")
-    return input('table: ')
-
-#def get database from user
-def getDatabase():
-    print("Which database would you like to use\n(A new database will be created if there is no database under the same name):")
-    databaseName = input('database: ').lower().strip() + '.db'
-    return databaseName
-
-def addTableOption(databaseName):
-    print(f'There is no table in {databaseName}. Would you like to create one: [y/n]:')
-    if input() == 'y':
-        database.createTable(databaseName, getTableNameInput())
-
-def lineBreak():
-    rows, columns = subprocess.check_output(['stty', 'size']).decode().split()
-    x = 0
-    line = ''
-    while x < int(columns):
-        line += '='
-        x += 1
-    print(f'{line}\n')
-
-def getNewDatabaseInput():
-    print('Which database would you like to switch to:')
-    databaseName = input('database: ').lower().strip() + '.db'
-    return databaseName
-
-    
-main()
---
+++ user.py
@@ -0,0 +1,76 @@
+import subprocess
+
+#Get input from user
+def addOneInput():
+    print('What is the first name you want to put into the table:')
+    firstName = input('first name: ')
+    lastName = input('last name: ')
+    email = input('email: ')
+    phone = input('phone: ')
+    return firstName, lastName, email, phone
+
+#Get input from user
+def deleteOneInput(databaseName, tableName):
+    print('Which record would you like to delete:')
+    return input('rowid: ')
+
+#Creates a list and get multiple input from user
+def addMultiple():
+    customerDict = []
+    userInput = 'y'
+    while userInput != 'n':
+        if userInput == 'y':
+            customerDict.append(addOneInput())
+            print(customerDict)
+        else:
+            print("Invalid Prompt")
+        print('Continue?[Y/N]:')
+        userInput = input().lower().strip()
+    return customerDict
+
+#Get input for columns and value from user
+def showInput():
+    print('What is the key you like to use to search\n(rowid, first_name, last_name, email, or phone):')
+    key = input()
+    print('What is the value you would like to search:')
+    value = input()
+    return key, value
+
+def updateInput():
+    print('What is the key you like to use to search\n(rowid, first_name, last_name, email, or phone):')
+    key = input()
+    print('What rowid you would like to use to update you record:')
+    rowid = input('rowid:')
+    print('What would you like to change the value to:')
+    value = input('value: ')
+    return key, value, rowid
+
+#get input for tables
+def getTableNameInput():
+    print("Which table would you like to look/create at:")
+    return input('table: ')
+
+#def get database from user
+def getDatabase():
+    print("Which database would you like to use\n(A new database will be created if there is no database under the same name):")
+    databaseName = input('database: ').lower().strip() + '.db'
+    return databaseName
+
+def addTableOption(databaseName):
+    print(f'There is no table in {databaseName}. Would you like to create one: [y/n]:')
+    if input() == 'y':
+        return True
+    else:
+        return False
+
+def lineBreak():
+    _, columns = subprocess.check_output(['stty', 'size']).decode().split()
+    line = ''
+    for _ in range(len(columns)):
+        line += '='
+    print(f'{line}\n')
+
+def getNewDatabaseInput():
+    print('Which database would you like to switch to:')
+    databaseName = input('database: ').lower().strip() + '.db'
+    return databaseName
